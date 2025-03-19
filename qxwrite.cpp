db91e173404013c941d15bca563fdc76f4ce81b4
qxares <ppvdbosch@hotmail.com>
2025-03-19 09:21:53 +0100

    Remove build artifacts from repo

--- qxwrite.cpp
+++ qxwrite.cpp
@@ -3,6 +3,7 @@
 #include <QMenuBar>
 #include <QMdiSubWindow>
 #include <QDebug>
+#include <QTimer>
 
 QxWrite::QxWrite(QWidget *parent) : QMainWindow(parent) {
     qDebug() << "QxWrite constructor starting...";
@@ -11,6 +12,10 @@ QxWrite::QxWrite(QWidget *parent) : QMainWindow(parent) {
     qDebug() << "MDI area created successfully.";
     setupMenus();
     resize(800, 600);
+    menuBar()->setVisible(true);  // Force here
+    QTimer::singleShot(100, this, [this]() {
+        qDebug() << "Menu bar visible after delay:" << menuBar()->isVisible();
+    });
     qDebug() << "QxWrite constructor finished.";
 }
 
@@ -23,7 +28,7 @@ void QxWrite::setupMenus() {
     fileMenu->addAction("Save");
     fileMenu->addSeparator();
     fileMenu->addAction("Exit", this, &QWidget::close);
-    menuBar()->setVisible(true);
+    menuBar()->setVisible(true);  // Force again
     qDebug() << "Menu bar is native:" << menuBar()->isNativeMenuBar();
     qDebug() << "Menu bar is visible:" << menuBar()->isVisible();
 }
