a8e28b6b655bc4e9ea27f9c3505fec09b6226cd1
qxares <ppvdbosch@hotmail.com>
2025-03-21 15:09:07 +0100

    Fix ToolBarBrick compile by adding QTextEdit include for toggle syncing

--- toolbarbrick.cpp
+++ toolbarbrick.cpp
@@ -8,23 +8,38 @@
 #include "iconbrick.h"
 #include <QToolBar>
 #include <QAction>
+#include <QTextEdit>
+#include <QTextCharFormat>
 #include <QDebug>
 
 ToolBarBrick::ToolBarBrick(QToolBar *toolBar, QTextEdit *edit, InsertBrick *insert, SaveBrick *save, BoldBrick *bold, NewFileBrick *newFile, ItalicBrick *italic, OpenFileBrick *openFile, IconBrick *iconBrick, QObject *parent)
-    : QObject(parent), toolBar(toolBar), targetEdit(edit), insertBrick(insert), saveBrick(save), boldBrick(bold), newFileBrick(newFile), italicBrick(italic), openFileBrick(openFile), iconBrick(iconBrick) {
+    : QObject(parent), toolBar(toolBar), targetEdit(edit), insertBrick(insert), saveBrick(save), boldBrick(bold), newFileBrick(newFile), italicBrick(italic), openFileBrick(openFile), iconBrick(iconBrick), boldAct(nullptr), italicAct(nullptr) {
     qDebug() << "ToolBarBrick initialized, toolBar:" << toolBar << ", target edit:" << targetEdit;
 
-    QAction *newAct = toolBar->addAction(iconBrick->getIcon("file-plus"), tr("New"), newFileBrick, &NewFileBrick::newFile);
-    QAction *openAct = toolBar->addAction(iconBrick->getIcon("open"), tr("Open"), openFileBrick, &OpenFileBrick::openFile);
-    QAction *saveAct = toolBar->addAction(iconBrick->getIcon("save"), tr("Save"), saveBrick, &SaveBrick::save);
+    toolBar->addAction(iconBrick->getIcon("file-plus"), tr("New"), newFileBrick, &NewFileBrick::newFile);
+    toolBar->addAction(iconBrick->getIcon("open"), tr("Open"), openFileBrick, &OpenFileBrick::openFile);
+    toolBar->addAction(iconBrick->getIcon("save"), tr("Save"), saveBrick, &SaveBrick::save);
     toolBar->addSeparator();
-    QAction *boldAct = toolBar->addAction(iconBrick->getIcon("bold"), tr("Bold"), boldBrick, &BoldBrick::toggleBold);
+    boldAct = toolBar->addAction(iconBrick->getIcon("bold"), tr("Bold"), boldBrick, &BoldBrick::toggleBold);
     boldAct->setCheckable(true);
-    QAction *italicAct = toolBar->addAction(iconBrick->getIcon("italic"), tr("Italic"), italicBrick, &ItalicBrick::toggleItalic);
+    italicAct = toolBar->addAction(iconBrick->getIcon("italic"), tr("Italic"), italicBrick, &ItalicBrick::toggleItalic);
     italicAct->setCheckable(true);
     toolBar->addSeparator();
-    QAction *insertImageAct = toolBar->addAction(iconBrick->getIcon("image"), tr("Insert Image"), insertBrick, &InsertBrick::insertImage);
+    toolBar->addAction(iconBrick->getIcon("image"), tr("Insert Image"), insertBrick, &InsertBrick::insertImage);
+
+    connect(targetEdit, &QTextEdit::cursorPositionChanged, this, &ToolBarBrick::updateToggleStates);
+    updateToggleStates(); // Initial state
 
     qDebug() << "Toolbar set up with icons and toggles.";
+}
+
+void ToolBarBrick::updateToggleStates() {
+    QTextCursor cursor = targetEdit->textCursor();
+    QTextCharFormat format = cursor.charFormat();
+    bool isBold = format.fontWeight() == QFont::Bold;
+    bool isItalic = format.fontItalic();
+    boldAct->setChecked(isBold);
+    italicAct->setChecked(isItalic);
+    qDebug() << "Toggle states updated - Bold:" << isBold << ", Italic:" << isItalic;
 }
 
