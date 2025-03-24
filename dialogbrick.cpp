bad7ed3a9638cc6161a089224769b5e3f60613c5
qxares <ppvdbosch@hotmail.com>
2025-03-24 12:13:04 +0100

    Fixed DialogBrick to use filter param, Insert Image now fully supports *.png *.jpg *.jpeg *.gif *.bmp

--- dialogbrick.cpp
+++ dialogbrick.cpp
@@ -7,20 +7,19 @@ DialogBrick::DialogBrick(QObject *parent) : QObject(parent) {
 }
 
 QString DialogBrick::getSaveFileName(const QString &caption, const QString &dir, const QString &filter) {
-    QString defaultFilter = "Markdown Files (*.md)";
-    QString selectedFile = QFileDialog::getSaveFileName(nullptr, caption, dir, "Markdown Files (*.md);;Text Files (*.txt);;HTML Files (*.html)", &defaultFilter, QFileDialog::DontUseNativeDialog);
-    if (selectedFile.isEmpty()) return "";
-    if (!selectedFile.endsWith(".md") && !selectedFile.endsWith(".txt") && !selectedFile.endsWith(".html")) {
-        selectedFile += ".md"; // Default to .md if no extension
+    QString fileName = QFileDialog::getSaveFileName(nullptr, caption, dir, filter);
+    if (!fileName.isEmpty()) {
+        qDebug() << "DialogBrick: Save selected:" << fileName;
     }
-    qDebug() << "DialogBrick: Save selected:" << selectedFile;
-    return selectedFile;
+    return fileName;
 }
 
 QString DialogBrick::getOpenFileName(const QString &caption, const QString &dir, const QString &filter) {
-    QString defaultFilter = "Markdown Files (*.md)";
-    QString selectedFile = QFileDialog::getOpenFileName(nullptr, caption, dir, "Markdown Files (*.md);;Text Files (*.txt);;HTML Files (*.html)", &defaultFilter, QFileDialog::DontUseNativeDialog);
-    qDebug() << "DialogBrick: Open selected:" << selectedFile;
-    return selectedFile;
+    QString fileName = QFileDialog::getOpenFileName(nullptr, caption, dir, filter);
+    if (!fileName.isEmpty()) {
+        qDebug() << "DialogBrick: Open selected:" << fileName;
+    } else {
+        qDebug() << "DialogBrick: Open selected: \"\"";
+    }
+    return fileName;
 }
-
