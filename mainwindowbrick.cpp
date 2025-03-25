3b02d605014306c4f684a5128be413249fe47604
qxares <ppvdbosch@hotmail.com>
2025-03-25 12:16:00 +0100

    Updated MainWindowBrick to spawn DocumentWindows for New and Open

--- mainwindowbrick.cpp
+++ mainwindowbrick.cpp
@@ -1,43 +1,44 @@
 #include "mainwindowbrick.h"
-#include <QVBoxLayout>
-#include <QToolBar>
+#include "documentwindow.h"
 #include <QDebug>
 
 MainWindowBrick::MainWindowBrick(QWidget *parent) : QMainWindow(parent) {
     qDebug() << "MainWindowBrick starting...";
-
-    setMinimumSize(800, 600);
-
-    QWidget *centralWidget = new QWidget(this);
-    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
-    textEdit = new QTextEdit(this);
-    layout->addWidget(textEdit);
-    setCentralWidget(centralWidget);
-
-    iconBrick = new IconBrick(this);
-    dialogBrick = new DialogBrick(this);
-    saveManager = new SaveManagerBrick(textEdit, this);
-    insertBrick = new InsertBrick(textEdit, dialogBrick, this);
-    boldBrick = new BoldBrick(textEdit, this);
-    newFileBrick = new NewFileBrick(textEdit, this);
-    italicBrick = new ItalicBrick(textEdit, this);
-    openFileBrick = new OpenFileBrick(textEdit, dialogBrick, this);
-    fontBrick = new FontBrick(textEdit, this);
-    colorBrick = new ColorBrick(textEdit, this);
-    alignLeftBrick = new AlignBrick(textEdit, AlignBrick::AlignLeft, this);
-    alignCenterBrick = new AlignBrick(textEdit, AlignBrick::AlignCenter, this);
-    alignRightBrick = new AlignBrick(textEdit, AlignBrick::AlignRight, this);
-
-    toolBar = new ToolBarBrick(this);
-    toolBar->setupToolBar(iconBrick, newFileBrick, openFileBrick, saveManager, boldBrick, italicBrick, fontBrick, colorBrick, insertBrick, alignLeftBrick, alignCenterBrick, alignRightBrick);  // Added alignment bricks
-    addToolBar(toolBar->getToolBar());
-
-    menuManager = new MenuManagerBrick(this);
-    menuManager->setupMenus(iconBrick, newFileBrick, openFileBrick, saveManager, boldBrick, italicBrick, fontBrick, colorBrick, insertBrick, alignLeftBrick, alignCenterBrick, alignRightBrick);  // Added alignment bricks
-    setMenuBar(menuManager->getMenuBar());
-
+    setupUI();
     qDebug() << "MainWindowBrick ready.";
 }
 
 MainWindowBrick::~MainWindowBrick() {
+    delete toolbarBrick;
+    delete menuManagerBrick;
+}
+
+void MainWindowBrick::setupUI() {
+    toolbarBrick = new ToolBarBrick(this);
+    addToolBar(toolbarBrick->getToolBar());
+
+    menuManagerBrick = new MenuManagerBrick(this);
+    setMenuBar(menuManagerBrick->getMenuBar());
+
+    // Connect "New" to spawn a new DocumentWindow
+    connect(toolbarBrick->getAction("new"), &QAction::triggered, this, [=]() {
+        qDebug() << "MainWindowBrick: Spawning new DocumentWindow";
+        DocumentWindow *doc = new DocumentWindow(nullptr);  // Independent window
+        doc->show();
+    });
+
+    // Connect "Open" to spawn a new DocumentWindow and trigger open
+    connect(toolbarBrick->getAction("open"), &QAction::triggered, this, [=]() {
+        qDebug() << "MainWindowBrick: Spawning DocumentWindow for Open";
+        DocumentWindow *doc = new DocumentWindow(nullptr);  // Independent window
+        doc->show();
+        OpenFileBrick *openBrick = doc->findChild<OpenFileBrick*>();
+        if (openBrick) {
+            openBrick->openFile();
+        } else {
+            qDebug() << "MainWindowBrick: Failed to find OpenFileBrick in DocumentWindow";
+        }
+    });
+
+    resize(400, 300);
 }
