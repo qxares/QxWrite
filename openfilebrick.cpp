0053ebb78ceaa2cb04439633b1e22a647f167f31
qxares <ppvdbosch@hotmail.com>
2025-03-25 15:37:21 +0100

    Fixed icon paths in icons.qrc, stabilized Open file loading with error logging

--- openfilebrick.cpp
+++ openfilebrick.cpp
@@ -5,28 +5,38 @@
 #include <QTextStream>
 #include <QDebug>
 
-OpenFileBrick::OpenFileBrick(QTextEdit *edit, QObject *parent) : QObject(parent), textEdit(edit) {
-    dialogBrick = new DialogBrick(this);
-    qDebug() << "OpenFileBrick initialized, target edit:" << textEdit;
+OpenFileBrick::OpenFileBrick(QTextEdit *edit, QObject *parent) 
+    : QObject(parent), m_textEdit(edit) {
+    m_dialog = new DialogBrick(this);
+    qDebug() << "OpenFileBrick initialized, target edit:" << m_textEdit;
 }
 
 void OpenFileBrick::openFile() {
-    qDebug() << "OpenFileBrick: openFile triggered, parent:" << parent();
-    QWidget *parentWidget = qobject_cast<QWidget*>(parent());
-    if (!parentWidget) {
-        qDebug() << "OpenFileBrick: Parent widget is null, using nullptr";
-        parentWidget = nullptr;
-    }
-    QString fileName = dialogBrick->getOpenFileName(parentWidget, "Open File", "", "Text Files (*.txt *.md);;All Files (*)");
+    QWidget *parentWidget = m_textEdit->parentWidget();
+    qDebug() << "OpenFileBrick: openFile triggered, parent:" << parentWidget;
+
+    QString fileName = m_dialog->getOpenFileName(
+        parentWidget,
+        "Open File",
+        "",
+        "Text Files (*.txt *.md);;All Files (*)"
+    );
+
     if (!fileName.isEmpty()) {
         QFile file(fileName);
         if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
             QTextStream in(&file);
-            textEdit->setText(in.readAll());
+            QString content = in.readAll();
+            if (!content.isEmpty()) {
+                m_textEdit->setPlainText(content);
+                qDebug() << "OpenFileBrick: Loaded file:" << fileName;
+            } else {
+                qDebug() << "OpenFileBrick: File is empty:" << fileName;
+            }
             file.close();
-            qDebug() << "OpenFileBrick: Loaded file:" << fileName;
         } else {
-            qDebug() << "OpenFileBrick: Failed to open file:" << fileName;
+            qDebug() << "OpenFileBrick: Failed to open file:" << fileName
+                     << "Error:" << file.errorString();
         }
     } else {
         qDebug() << "OpenFileBrick: No file selected";
--- toolbarbrick.cpp
+++ toolbarbrick.cpp
@@ -7,12 +7,24 @@ ToolBarBrick::ToolBarBrick(QWidget *parent) : QObject(parent) {
     toolBar = new QToolBar("Main Toolbar", parent);
     toolBar->setMovable(false);
 
-    newAction = new QAction(QIcon(":/icons/new.png"), "New", this);
+    newAction = new QAction(QIcon(":/icons/icons/new.png"), "New", this);
     newAction->setToolTip("New Document");
+    if (!newAction->icon().isNull()) {
+        qDebug() << "ToolBarBrick: New icon loaded successfully";
+    } else {
+        qDebug() << "ToolBarBrick: Failed to load icons/new.png";
+        newAction->setIcon(QIcon::fromTheme("document-new")); // Fallback
+    }
     toolBar->addAction(newAction);
 
-    openAction = new QAction(QIcon(":/icons/open.png"), "Open", this);
+    openAction = new QAction(QIcon(":/icons/icons/open.png"), "Open", this);
     openAction->setToolTip("Open Document");
+    if (!openAction->icon().isNull()) {
+        qDebug() << "ToolBarBrick: Open icon loaded successfully";
+    } else {
+        qDebug() << "ToolBarBrick: Failed to load icons/open.png";
+        openAction->setIcon(QIcon::fromTheme("document-open")); // Fallback
+    }
     toolBar->addAction(openAction);
 
     qDebug() << "ToolBarBrick initialized with toolbar:" << toolBar;
