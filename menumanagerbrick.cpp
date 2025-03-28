2dcf0b2400c8e55ad796a60c70e6cf3e82048049
qxares <ppvdbosch@hotmail.com>
2025-03-28 08:21:56 +0100

    Remove icons from menu bar items, keep toolbar icons - cleaner look

--- menumanagerbrick.cpp
+++ menumanagerbrick.cpp
@@ -18,25 +18,25 @@ void MenuManagerBrick::setupMenus(QAction *openAction, QAction *saveAction,
                                   QAction *boldAction, QAction *italicAction, QAction *fontAction,
                                   QAction *colorAction, QAction *imageAction, QAction *alignLeftAction,
                                   QAction *alignCenterAction, QAction *alignRightAction) {
-    QAction *newActionMenu = fileMenu->addAction(QIcon(":/icons/icons/plus.svg"), "New");
+    QAction *newActionMenu = fileMenu->addAction("New");  // No icon
     QMenu *newMenu = new QMenu();
-    QAction *newNote = newMenu->addAction(QIcon(":/icons/icons/plus.svg"), "QxNote");
-    QAction *newDoc = newMenu->addAction(QIcon(":/icons/icons/plus.svg"), "QxDocument");
-    QAction *newSheet = newMenu->addAction(QIcon(":/icons/icons/plus.svg"), "QxSheet");
+    QAction *newNote = newMenu->addAction("QxNote");      // No icon
+    QAction *newDoc = newMenu->addAction("QxDocument");   // No icon
+    QAction *newSheet = newMenu->addAction("QxSheet");    // No icon
     newActionMenu->setMenu(newMenu);
 
-    QAction *saveAsAction = fileMenu->addAction(QIcon(":/icons/icons/save.svg"), "Save As");
-    if (openAction) fileMenu->addAction(openAction);
-    if (saveAction) fileMenu->addAction(saveAction);
-    if (boldAction) formatMenu->addAction(boldAction);
-    if (italicAction) formatMenu->addAction(italicAction);
-    if (fontAction) formatMenu->addAction(fontAction);
-    if (colorAction) formatMenu->addAction(colorAction);
-    if (imageAction) editMenu->addAction(imageAction);
+    QAction *saveAsAction = fileMenu->addAction("Save As");  // No icon
+    if (openAction) fileMenu->addAction(openAction);         // Keeps toolbar icon
+    if (saveAction) fileMenu->addAction(saveAction);         // Keeps toolbar icon
+    if (boldAction) formatMenu->addAction(boldAction);       // Keeps toolbar icon
+    if (italicAction) formatMenu->addAction(italicAction);   // Keeps toolbar icon
+    if (fontAction) formatMenu->addAction(fontAction);       // Keeps toolbar icon
+    if (colorAction) formatMenu->addAction(colorAction);     // Keeps toolbar icon
+    if (imageAction) editMenu->addAction(imageAction);       // Keeps toolbar icon
     formatMenu->addSeparator();
-    if (alignLeftAction) formatMenu->addAction(alignLeftAction);
-    if (alignCenterAction) formatMenu->addAction(alignCenterAction);
-    if (alignRightAction) formatMenu->addAction(alignRightAction);
+    if (alignLeftAction) formatMenu->addAction(alignLeftAction);     // Keeps toolbar icon
+    if (alignCenterAction) formatMenu->addAction(alignCenterAction); // Keeps toolbar icon
+    if (alignRightAction) formatMenu->addAction(alignRightAction);   // Keeps toolbar icon
 
     QObject::connect(newNote, &QAction::triggered, this, [this]() { emit newFileTriggered(0); });
     QObject::connect(newDoc, &QAction::triggered, this, [this]() { emit newFileTriggered(1); });
