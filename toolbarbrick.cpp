70f6d80f6c088256031be4a90063dd4c646cde4c
qxares <ppvdbosch@hotmail.com>
2025-03-28 10:34:05 +0100

    Fix icon paths in toolbarbrick to match icons.qrc structure

--- toolbarbrick.cpp
+++ toolbarbrick.cpp
@@ -3,17 +3,17 @@
 
 ToolBarBrick::ToolBarBrick(QWidget *parent) : QObject(parent) {
     toolBar = new QToolBar(parent);
-    newAction = addAction("new", "New", ":/icons/plus.svg");
-    openAction = addAction("open", "Open", ":/icons/open.svg");
-    saveAction = addAction("save", "Save", ":/icons/save.svg");
-    boldAction = addAction("bold", "Bold", ":/icons/bold.svg");
-    italicAction = addAction("italic", "Italic", ":/icons/italic.svg");
-    fontAction = addAction("font", "Font", ":/icons/font.svg");
-    colorAction = addAction("color", "Color", ":/icons/color.svg");
-    imageAction = addAction("image", "Insert Image", ":/icons/image.svg");
-    alignLeftAction = addAction("alignLeft", "Align Left", ":/icons/align-left.svg");
-    alignCenterAction = addAction("alignCenter", "Align Center", ":/icons/align-center.svg");
-    alignRightAction = addAction("alignRight", "Align Right", ":/icons/align-right.svg");
+    newAction = addAction("new", "New", ":/icons/icons/plus.svg");
+    openAction = addAction("open", "Open", ":/icons/icons/open.svg");
+    saveAction = addAction("save", "Save", ":/icons/icons/save.svg");
+    boldAction = addAction("bold", "Bold", ":/icons/icons/bold.svg");
+    italicAction = addAction("italic", "Italic", ":/icons/icons/italic.svg");
+    fontAction = addAction("font", "Font", ":/icons/icons/font.svg");
+    colorAction = addAction("color", "Color", ":/icons/icons/color.svg");
+    imageAction = addAction("image", "Insert Image", ":/icons/icons/image.svg");
+    alignLeftAction = addAction("alignLeft", "Align Left", ":/icons/icons/align-left.svg");
+    alignCenterAction = addAction("alignCenter", "Align Center", ":/icons/icons/align-center.svg");
+    alignRightAction = addAction("alignRight", "Align Right", ":/icons/icons/align-right.svg");
     tableAction = addAction("table", "Insert Table", ""); // No icon yet
     qDebug() << "ToolBarBrick initialized with toolbar:" << toolBar;
 }
@@ -41,8 +41,13 @@ QAction* ToolBarBrick::getAction(const QString &name) const {
 QAction* ToolBarBrick::addAction(const QString &name, const QString &text, const QString &iconPath) {
     QAction *action = new QAction(text, this);
     if (!iconPath.isEmpty()) {
-        action->setIcon(QIcon(iconPath));
-        qDebug() << "ToolBarBrick:" << text << "icon loaded from" << iconPath;
+        QIcon icon(iconPath);
+        if (icon.isNull()) {
+            qDebug() << "ToolBarBrick:" << text << "icon failed to load from" << iconPath;
+        } else {
+            action->setIcon(icon);
+            qDebug() << "ToolBarBrick:" << text << "icon loaded from" << iconPath;
+        }
     } else {
         qDebug() << "ToolBarBrick:" << text << "no icon specified";
     }
